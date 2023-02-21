1c1dfeae9c11c073659c3faf80b423e406d8be12
Arthur <arthur.pulfermuller@gmail.com>
2023-02-21 16:02:34 +0100

    working on slideshow

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,39 +1,21 @@
 import React from "react";
 
-import getData from "../assets/database/projects.json";
 import tvImg from "../assets/images/television.png";
+import getData from "../assets/database/projects.json";
 import Slideshow from "./Slideshow";
 
-// {
-//   console.log(getData.projects[0].image);
-// }
-
 const Portfolio = () => {
-  const data = getData.projects.find((data) => data);
-  {console.log(data);}
+
+
   return (
     <div className="portfolio">
       <h2>PORTFOLIO</h2>
       <div className="bar"></div>
       <div className="tv">
         <img className="tvBorder" src={tvImg} alt="cadre tv" />
-        <button className="previousBtn"></button>
-        <div className="projects">
-          <img className="projectsImg" src={data.image} alt="lala" />
-          <p className="portofolioText">{data.description}</p>
-        </div>
-        {/* <ul className="flexCard">
-        {getData.projects.map((projects, id) => (
-          <div>
-            <li className="projects">
-              <img className="projectsImg" src={projects.image} alt="lala" />
-              <p className="portfolioText" key={id}>
-                {projects.description}
-              </p>
-            </li>
-          </div>
-        ))}
-      </ul> */}
+      <Slideshow data={getData}
+      />
+
       </div>
     </div>
   );
--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,20 +1,56 @@
 import React, { useState } from "react";
 
-import getData from "../assets/database/projects.json";
+const Slideshow = (props) => {
+  const [indexOfProjects, setIndexOfProjects] = useState({
+    index: 0,
+    description: 0,
+  });
 
-const Slideshow = () => {
-  const [currentImg, SetcurrentImg] = useState([{ index: 1, counter: 1 }]);
+  const handleNextBtn = () => {
+    if (indexOfProjects.index >= props.length - 1) {
+      return setIndexOfProjects({
+        index: 0,
+        description: 0,
+      });
+    } else {
+      indexOfProjects.index++;
+      indexOfProjects.description++;
 
-  const data = getData.projects.find((data) => data);
+      return setIndexOfProjects({
+        index: indexOfProjects.index,
+        description: indexOfProjects.description,
+      });
+    }
+  };
+
+  const handlePreviousBtn = () => {
+    if (indexOfProjects.index <= 0) {
+      return setIndexOfProjects({
+        index: props.length - 1,
+        description: props.length - 1,
+      });
+    } else {
+      indexOfProjects.index--;
+      indexOfProjects.description--;
+
+      return setIndexOfProjects({
+        index: indexOfProjects.index,
+        description: indexOfProjects.description,
+      });
+    }
+  };
 
   return (
-    <div className="slideshow">
-        {/* {getData.projects.map((projects, id) => (
-          <div>
-              <img key={id} className="projectsImg" src={projects.image} alt="lala" />
-          </div>
-        ))} */}
-        <img className="projectsImg" src={data.image[currentImg.index]} alt="lala" />
+    <div className="projects">
+      <button className="previousBtn" onClick={handlePreviousBtn}></button>
+      {props.data.map((data) => (
+        <div>
+          <img src={data.image} alt={data.title} />
+          <div>{data.description}</div>
+        </div>
+      ))}
+      <button className="previousBtn" onClick={handleNextBtn}></button>
+      <p className="portofolioText"></p>
     </div>
   );
 };
