620ab7f0ca8ae9eeec2d77910e39043072241c27
Arthur <arthur.pulfermuller@gmail.com>
2023-02-22 12:27:19 +0100

    slideshow functional

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-import tvImg from "../assets/images/television.png";
-import getData from "../assets/database/projects.json";
 import Slideshow from "./Slideshow";
 
+import tvImg from "../assets/images/television.png";
+
 const Portfolio = () => {
-  const data = getData.projects.find((data) => data);
-console.log(getData.projects[1].image);
+  // const data = getData.projects.find((data) => data);
+// console.log(getData.projects[1].image);
+
 
   return (
     
@@ -15,12 +16,7 @@ console.log(getData.projects[1].image);
       <div className="bar"></div>
       <div className="tv">
         <img className="tvBorder" src={tvImg} alt="cadre tv" />
-        <div>
-        {/* {getData.map((image, id) => (
-          <Slideshow key={id} image={image} />
-        ))} */}
-        <Slideshow data={data} image={data.image} description={data.description} />
-        </div>
+        <Slideshow />
       </div>
     </div>
   );
--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,55 +1,60 @@
 import React, { useState } from "react";
+import getData from "../assets/database/projects.json";
 
 const Slideshow = (props) => {
-  const [indexOfProjects, setIndexOfProjects] = useState({
-    index: 0,
-    description: 0,
-  });
+  const data = getData;
+
+  const filterImg = data.projects.map((obj) => obj.image);
+  const filterDescription = data.projects.map((obj) => obj.description);
+  const filterTitle = data.projects.map((obj) => obj.title);
+
+  const [indexProjects, setIndexProjects] = useState({ index: 1 });
 
   const handleNextBtn = () => {
-    if (indexOfProjects.index >= props.length - 1) {
-      return setIndexOfProjects({
+    if (
+      (indexProjects.index >= filterImg.length - 1) &
+      (filterDescription.length - 1)
+    ) {
+      return setIndexProjects({
         index: 0,
-        description: 0,
+        // description: 0,
       });
     } else {
-      indexOfProjects.index++;
-      indexOfProjects.description++;
+      indexProjects.index++;
+      // indexOfProjects.description++;
 
-      return setIndexOfProjects({
-        index: indexOfProjects.index,
-        description: indexOfProjects.description,
+      return setIndexProjects({
+        index: indexProjects.index,
+        // description: indexOfProjects.description,
       });
     }
   };
 
   const handlePreviousBtn = () => {
-    if (indexOfProjects.index <= 0) {
-      return setIndexOfProjects({
-        index: props.length - 1,
-        description: props.length - 1,
+    if (indexProjects.index <= 0) {
+      return setIndexProjects({
+        index: (filterImg.length - 1) & (filterDescription.length - 1),
+        // description: props.length - 1,
       });
     } else {
-      indexOfProjects.index--;
-      indexOfProjects.description--;
+      indexProjects.index--;
+      // indexProjects.description--;
 
-      return setIndexOfProjects({
-        index: indexOfProjects.index,
-        description: indexOfProjects.description,
+      return setIndexProjects({
+        index: indexProjects.index,
       });
     }
   };
 
   return (
     <div className="projects">
-      <button className="previousBtn"  onClick={handlePreviousBtn}></button>
-      {console.log(handlePreviousBtn)}
-        <div>
-          <img src={props.image} alt={props.title} />
-          <div>{props.description}</div>
-        </div>
+      <button className="previousBtn" onClick={handlePreviousBtn}></button>
+      <div>
+        <img className="projectsImg" src={filterImg[indexProjects.index]} alt={filterTitle[indexProjects.index]} />
+       <p className="projectsDescription" >{filterDescription[indexProjects.index]}</p>
+      </div>
       <button className="previousBtn" onClick={handleNextBtn}></button>
-      {console.log(handleNextBtn)}
+      {/* {console.log(setIndexProjects)} */}
       <p className="portofolioText"></p>
     </div>
   );
